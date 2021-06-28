644d6cdf20faa50e24e3c66897a8353fe51ccd69
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-28 04:59:54 +0000

    [ADD] methods all, show, and update

--- console.py
+++ console.py
@@ -2,12 +2,13 @@
 """Define HBNBCommand Class"""
 import cmd
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from models import storage
 
 
 def parse(arg):
     """Return an argument tuple"""
     return(tuple(arg.split()))
+
 
 class HBNBCommand(cmd.Cmd):
     """Command Interpreter"""
@@ -59,8 +60,68 @@ class HBNBCommand(cmd.Cmd):
         elif "{}.{}".format(tuparg[0], tuparg[1]) not in dicObj:
             print("** no instance found **")
         else:
-            print(dicOBJ["{}.{}".format(tuparg[0], tuparg[1])])
+            print(dicObj["{}.{}".format(tuparg[0], tuparg[1])])
 
+    def do_destroy(self, arg):
+        """Usage: destroy <class> <id>
+        Deletes an instance based on the class name and id"""
+        tuparg = parse(arg)
+        dicObj = storage.all()
+        if len(tuparg) == 0:
+            print("** class name missing **")
+        elif tuparg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(tuparg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(tuparg[0], tuparg[1]) not in dicObj:
+            print("** no instance found **")
+        else:
+            del dicObj["{}.{}".format(tuparg[0], tuparg[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        """Usage: all or all <class>
+        Prints all string representation of all instances based
+        or not on the class name"""
+        tuparg = parse(arg)
+        if len(tuparg) > 0 and tuparg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            l = []
+            for val in storage.all().values():
+                if len(tuparg) > 0 and tuparg[0] == val.__class__.__name__:
+                    l.append(val.__str__())
+                elif len(tuparg) == 0:
+                    l.append(val.__str__())
+            print(l)
+
+    def do_update(self, arg):
+        """Usage: update <class> <id> <attribute_name> <attribute_value>
+        Updates an instance based on the class name and id by adding
+        or updating attribute"""
+        tuparg = parse(arg)
+        dicObj = storage.all()
+
+        if len(tuparg) == 0:
+            print("** class name missing **")
+        elif tuparg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(tuparg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(tuparg[0], tuparg[1]) not in dicObj:
+            print("** no instance found **")
+        elif len(tuparg) == 2:
+            print("** attribute name missing **")
+        elif len(tuparg) == 3:
+            print("** value missing **")
+        else:
+            obj = dicObj["{}.{}".format(tuparg[0], tuparg[1])]
+            if tuparg[2] in obj.__class__.__dict__.keys():
+                typeval = type(obj.__class__.__dict__[tuparg[2]])
+                obj.__dict__[tuparg[2]] = typeval(tuparg[3])
+            else:
+                obj.__dict__[tuparg[2]] = tuparg[3]
+            storage.save()
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
