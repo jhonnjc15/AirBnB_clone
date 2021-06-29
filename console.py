d460c2a1eb7c1d5959e58321eadbb543738e24b3
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-29 22:17:55 +0000

    [UPDATE] parse

--- console.py
+++ console.py
@@ -14,8 +14,22 @@ import re
 
 
 def parse(arg):
-    """Return an argument tuple"""
-    return(arg.split())
+    """Return list of arg"""
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
@@ -146,6 +160,16 @@ class HBNBCommand(cmd.Cmd):
                     return(dic[c[0]](line))
         print("*** Unknown syntax: {}".format(arg))
         return(False)
+
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        listarg = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
 
 
 if __name__ == "__main__":
