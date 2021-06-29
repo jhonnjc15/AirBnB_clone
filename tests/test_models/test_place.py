e503de79ffa4fdce1c57a9e6813392fd61ff041c
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-29 17:07:50 +0000

    [ADD] test more cases

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines unittests for place.py"""
 
+from models.base_model import BaseModel
 from models.place import Place
 import unittest
 
@@ -20,6 +21,100 @@ class TestPlace(unittest.TestCase):
         place = Place()
         self.assertTrue(hasattr(place, "city_id"))
         self.assertEqual(place.city_id, "")
+
+    def test_user_id(self):
+        """Test if Place has attr user_id and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "user_id"))
+        self.assertEqual(place.user_id, "")
+
+    def test_name(self):
+        """Test if Place has attr name and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "name"))
+        self.assertEqual(place.name, "")
+
+    def test_description(self):
+        """Test if Place has attr description and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "description"))
+        self.assertEqual(place.description, "")
+
+    def test_number_rooms(self):
+        """Test if Place has attr number_rooms and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "number_rooms"))
+        self.assertEqual(type(place.number_rooms), int)
+        self.assertEqual(place.number_rooms, 0)
+
+    def test_number_bathrooms(self):
+        """Test if Place has attr number_bathrooms and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        self.assertEqual(type(place.number_bathrooms), int)
+        self.assertEqual(place.number_bathrooms, 0)
+
+    def test_max_guest(self):
+        """Test if Place has attr max_guest and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "max_guest"))
+        self.assertEqual(type(place.max_guest), int)
+        self.assertEqual(place.max_guest, 0)
+
+    def test_price_by_night(self):
+        """Test if Place has attr price_by_night and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "price_by_night"))
+        self.assertEqual(type(place.price_by_night), int)
+        self.assertEqual(place.price_by_night, 0)
+
+    def test_latitude(self):
+        """Test if Place has attr latitude and it's a float == 0.0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "latitude"))
+        self.assertEqual(type(place.latitude), float)
+        self.assertEqual(place.latitude, 0.0)
+
+    def test_longitude(self):
+        """Test if Place has attr longitude and it's a float == 0.0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "longitude"))
+        self.assertEqual(type(place.longitude), float)
+        self.assertEqual(place.longitude, 0.0)
+
+    def test_amenity_ids(self):
+        """Test of Place has attr amenity_ids and it's an empty list"""
+        place = Place()
+        self.assertTrue(hasattr(place, "amenity_ids"))
+        self.assertEqual(type(place.amenity_ids), list)
+        self.assertEqual(len(place.amenity_ids), 0)
+
+    def test_create_dict(self):
+        """Test if to_dict method create a dic with proper attributes"""
+        p = Place()
+        dic = p.to_dict()
+        self.assertEqual(type(dic), dict)
+        for att in p.__dict__:
+            self.assertTrue(att in dic)
+            self.assertTrue("__class__" in dic)
+
+    def test_dict_values(self):
+        """Test if values of dict returned from to_dict are correct"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        p = Place()
+        dic = p.to_dict()
+        self.assertEqual(dic["__class__"], "Place")
+        self.assertEqual(type(dic["created_at"]), str)
+        self.assertEqual(type(dic["updated_at"]), str)
+        self.assertEqual(dic["created_at"], p.created_at.strftime(t_format))
+        self.assertEqual(dic["updated_at"], p.updated_at.strftime(t_format))
+
+    def test_str(self):
+        """Test str method"""
+        p = Place()
+        string = "[Place] ({}) {}".format(p.id, p.__dict__)
+        self.assertEqual(string, str(p))
+
 
 if __name__ == "__main__":
     unittest.main()
