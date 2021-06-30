8f2096b645d311acbcf6884e3587da8e6f646fb2
Alex Rivera <alexriveracruz4@gmail.com>
2021-06-30 00:13:28 +0000

    [FIX] quit init

--- models/user.py
+++ models/user.py
@@ -13,5 +13,5 @@ class User(BaseModel):
     first_name = ""
     last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__()
+    #def __init__(self, *args, **kwargs):
+        #super().__init__()
